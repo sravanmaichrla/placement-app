97cacad525f30dc807353c8d329a0ab3e02190f0
sravanmaicharla <sravankumar99633@gmail.com>
2025-01-07 10:52:14 +0530

    second commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,45 @@
-import { GoogleLogin, googleLogout, useGoogleLogin } from '@react-oauth/google';
+import '../App.css'
 
-import React from 'react'
+import { GoogleLogin, googleLogout, useGoogleLogin } from '@react-oauth/google';
+import React, { useState } from 'react';
+
 import { jwtDecode } from "jwt-decode";
-import { useState } from 'react';
 
 function Login() {
-    // const [showLogin, setShowLogin] = useState(false);
-    // const responseSucess = (credentialResponse) => {
-    //     setShowLogin(true);
-    //     const decoded = jwtDecode(credentialResponse.credential);
-    //     console.log(decoded);
-    //     // setShowLogout(true);
-    //   };
-    
-    //   const responseFailure = () => {
-    //     console.log("login is not sucesss");
-    //   };
-    
-    //   const Logout = () => {
-    //     setShowLogin(true);
-    //   };
+  const [showLogin, setShowLogin] = useState(false);
+  const [decoded, setDecoded] = useState(null);
+
+  const responseSuccess = (credentialResponse) => {
+    setShowLogin(true);
+    const decoded = jwtDecode(credentialResponse.credential);
+    setDecoded(decoded);
+    console.log(decoded);
+  };
+
+  const responseFailure = () => {
+    console.log("login is not successful");
+  };
+
+  const handleLogout = () => {
+    setShowLogin(false);
+    setDecoded(null);
+    googleLogout();
+  };
 
   return (
     <div>
+      {showLogin ? (
+        <div className='userWelcome'>
+          <h1>Welcome, {decoded.name}</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
         <GoogleLogin
-    onSuccess={credentialResponse => {
-        const decoded = jwtDecode(credentialResponse.credential);
-      console.log(decoded);
-    }}
-    onError={() => {
-      console.log('Login Failed');
-    }}
-  />;
+          onSuccess={responseSuccess}
+          onError={responseFailure}
+        />
+      )}
     </div>
-      
   )
 }
 
